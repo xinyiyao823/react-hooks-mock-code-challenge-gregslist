15b0a7d028d9617bd5b5d88ba3cc1c4b0c281b86
+ <+>
2021-09-28 19:13:26 -0500

    commit tree baca1c9e29facb136d56f7f21d90129f8f2d6869

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Search() {
+function Search({search, setSearch}) {
   function handleSubmit(e) {
     e.preventDefault();
-    console.log("submitted");
+    setSearch('');
   }
 
   return (
@@ -12,8 +12,8 @@ function Search() {
         type="text"
         id="search"
         placeholder="search free stuff"
-        value={""}
-        onChange={(e) => console.log(e.target.value)}
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
       <button type="submit">🔍</button>
     </form>
