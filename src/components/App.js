b7e831f925f29baae01f1263b6a6eacdf92f2a41
+ <+>
2021-09-28 19:13:40 -0500

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  //GET request
+  const [listings, setListings] = useState([])
+// Search Bar
+const [search, setSearch] = useState('')
+  useEffect(() => {
+    fetch('http://localhost:6001/listings')
+    .then(r => r.json())
+    .then(data => setListings(data))
+  }, [])
+  
+  function handleDelete(id) {
+    fetch(`http://localhost:6001/listings/${id}`, {
+      method: 'DELETE',
+    })
+    const listingsToDisplay = listings.filter(listing => listing.id !== id)
+    setListings(listingsToDisplay)
+  }
+
+  const searchedItems = listings.filter(listing => listing.description.toLowerCase().includes(search.toLowerCase()))
+
+  
+  
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header search={search} setSearch={setSearch}/>
+      <ListingsContainer listings={searchedItems} handleDelete={handleDelete}/>
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,18 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({listings, handleDelete}) {
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listings.map(listing => <ListingCard
+        key={listing.id}
+        id={listing.id}
+        description={listing.description}
+        image={listing.image}
+        location={listing.location}
+        handleDelete={handleDelete}
+        />)}
       </ul>
     </main>
   );
